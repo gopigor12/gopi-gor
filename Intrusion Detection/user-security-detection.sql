1d0cb9b775a87a660b888a3461b9d291e2927ca3
Gopi Gor <151388464+gopigor12@users.noreply.github.com>
2024-05-21 19:24:41 -0400

    Update user-security-detection.sql

--- Intrusion Detection/user-security-detection.sql
+++ Intrusion Detection/user-security-detection.sql
@@ -1,3 +1,11 @@
+/* Successful Logins from Unusual Location 
+ 
+This discussion covers a new query that has been created to detect successful logins from unusual locations 
+and then when this query is run, it will give results about all those users and devices that have been accessed 
+from outside the country. This means there could be employees outside the country who work, but their IP 
+addresses would not be considered malicious once the configuration is set to trust them. Even for testing 
+purposes, if they are found to be outside, their IP address can always be verified. */
+
 _sourceCategory=Labs/Okta 
 | parse "\"ipAddress\":\"*.*.*.*\",\"geographicalContext\":{\"country\":\"*\"}" as source_ip, 
 country 
