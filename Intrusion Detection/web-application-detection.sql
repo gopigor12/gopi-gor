81781d871866144cca7ba44be44113c1e98c9f03
Gopi Gor <151388464+gopigor12@users.noreply.github.com>
2024-05-21 19:29:19 -0400

    Update web-application-detection.sql

--- Intrusion Detection/web-application-detection.sql
+++ Intrusion Detection/web-application-detection.sql
@@ -6,7 +6,16 @@ which then returns status code between 400 – 500. The incomplete POST requests
 which can have information from the previous request. The detection query for this has been shown and 
 explained */
 
-_sourceCategory=Labs/Apache/* 
+_sourceCategory=Labs/Apache/"*"
 | parse regex field=_raw "(?<raw_log>.*)" 
 | parse regex field=raw_log "(?i)content-length:\s*(?<header_size>\d+)" 
 | where method == "POST" AND (header_size > 500 OR status_code == "413")
+
+/*First, we select the source category as the log source we want to use in this query. Now, to get all the HTTP 
+header details, we need to look at the raw log source. In the second line, a new field called raw_log is created, 
+and the complete raw log message is extracted. The next line looks for the case-insensitive part in content 
+length with (?i) and gathers the numeric value into a new field called header_size. Finally, as per the 
+vulnerability, the attack occurs on POST requests, so we use the where clause to look for POST requests in 
+the “method” field and filter on the basis of header size > 500 or status_code = 413. We want to make sure 
+that we get all POST requests that may have an unexpectedly big header size or a status code of 413 to 
+denote a large payload.*/
